0f38e8a9398e44d8904dfddd4e0bc27130e0b6ef
sofiane berkane <sofianeberkane202@gmail.com>
2025-03-31 01:17:51 +0100

    refactor: add favicon, update build scripts, and enhance backend error handling

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,20 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
+import path from "path";
+import AppError from "./utils/AppError.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Allow frontend origin
+    credentials: true, // REQUIRED to allow cookies
+  })
+);
 
 app.use((req, res, next) => {
   req.requestedAt = new Date().toISOString();
@@ -22,6 +30,19 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/notifications", notificationRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 app.use(generateError);
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  build: {
+    rollupOptions: {
+      plugins: [visualizer({ open: true })],
+    },
+  },
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000//api/v1",
+        target: "http://localhost:8000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, "/api/v1"), // ✅ Correct
       },
     },
   },
